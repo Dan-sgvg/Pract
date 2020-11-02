86c86076064b4a77eb3bacc5bcaeaf75d021e1df
Dan-sgvg <73816800+Dan-sgvg@users.noreply.github.com>
2020-11-02 13:27:43 +0300

    end first task

--- TDM_Task_Number_01/Program.cs
+++ TDM_Task_Number_01/Program.cs
@@ -10,7 +10,22 @@ namespace TDM_Task_Number_01
     {
         static void Main(string[] args)
         {
+            System.Console.WriteLine("Enter amount of gold:");
+            int amount_of_gold = System.Convert.ToInt32(System.Console.ReadLine());
+            int crystal_cost = 20;
+            System.Console.WriteLine($"Crystal cost = {crystal_cost}");
 
+            System.Console.WriteLine("How many crystals you want to buy?");
+            int amount_of_crystals = System.Convert.ToInt32(System.Console.ReadLine());
+
+            amount_of_gold = amount_of_gold - amount_of_crystals * crystal_cost;
+
+            if (amount_of_gold <= 0)
+                System.Console.WriteLine("You need more gold.");
+            else
+                System.Console.WriteLine($"You have {amount_of_gold} gold\nand {amount_of_crystals} crystals.");
+
+            System.Convert.ToInt32(System.Console.ReadLine());
         }
     }
 }

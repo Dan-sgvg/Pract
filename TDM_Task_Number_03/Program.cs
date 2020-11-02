10562be3708d4f7d94eea17beeb0a5202edf01fe
Dan-sgvg <73816800+Dan-sgvg@users.noreply.github.com>
2020-11-02 14:32:48 +0300

    3 задание

---
+++ TDM_Task_Number_03/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDM_Task_Number_03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string expression;
+            int num_of_try = 0;
+            do
+            {
+                expression = Console.ReadLine();
+                num_of_try += 1;
+                if (expression.Equals("dr.mom"))
+                {
+                    Console.WriteLine("Secret massage!!");
+                    break;
+                }
+            } while (num_of_try != 3);
+            if (num_of_try == 3)
+                Console.WriteLine("limit is exceeded!!");
+
+            Console.ReadLine();
+        }
+    }
+}
